13fd1710c1865c5202c78a8bc37fa1e2a1a5bb56
TVCnotFTE <126646555+TVCnotFTE@users.noreply.github.com>
2023-03-08 12:20:09 -0600

    Update simple-interest.sh
    Updating personal contact details.

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors:  David Steen
+   # david-steen@bethel.edu
 
    # Input:
    # p, principal amount
